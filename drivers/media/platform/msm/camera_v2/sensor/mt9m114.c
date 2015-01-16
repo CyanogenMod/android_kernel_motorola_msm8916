7674d205ced768813ff5e2f28d7229e2abc98b35
Vasko Kalanoski <vaskok@codeaurora.org>
2015-01-16 15:40:46 +0200

    msm: camera: check size of allocated buffer to prevent overflow
    Add check of size for allocated buffer in mt9m114 sensor driver to
    prevent overflow.
    
    Change-Id: If18c71d57eb881382a56c32b66d1654c3b802c04
    Signed-off-by: Vasko Kalanoski <vaskok@codeaurora.org>

--- drivers/media/platform/msm/camera_v2/sensor/mt9m114.c
+++ drivers/media/platform/msm/camera_v2/sensor/mt9m114.c
@@ -1484,6 +1484,14 @@ int32_t mt9m114_sensor_config(struct msm_sensor_ctrl_t *s_ctrl,
 			break;
 		}
 
+		if (!conf_array.size ||
+			conf_array.size > I2C_SEQ_REG_DATA_MAX) {
+
+			pr_err("%s:%d failed\n", __func__, __LINE__);
+			rc = -EFAULT;
+			break;
+		}
+
 		reg_setting = kzalloc(conf_array.size *
 			(sizeof(struct msm_camera_i2c_reg_array)), GFP_KERNEL);
 		if (!reg_setting) {
@@ -1775,6 +1783,14 @@ int32_t mt9m114_sensor_config32(struct msm_sensor_ctrl_t *s_ctrl,
 		conf_array.delay = conf_array32.delay;
 		conf_array.size = conf_array32.size;
 		conf_array.reg_setting = compat_ptr(conf_array32.reg_setting);
+
+		if (!conf_array.size ||
+			conf_array.size > I2C_SEQ_REG_DATA_MAX) {
+
+			pr_err("%s:%d failed\n", __func__, __LINE__);
+			rc = -EFAULT;
+			break;
+		}
 
 		reg_setting = kzalloc(conf_array.size *
 			(sizeof(struct msm_camera_i2c_reg_array)), GFP_KERNEL);
