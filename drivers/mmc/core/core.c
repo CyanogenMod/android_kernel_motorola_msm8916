02e45ddd5721a000e69c15187519e1f3671b38b4
Ramsudharsan <ramsudharsanm@gmail.com>
2015-11-08 10:23:03 +0100

    drivers: mmc: disable software CRC checks for 30% performance
    Change-Id: I1e9ba44cd0941a7c9390368f24470077c79eca4a
    Signed-off-by: Ramsudharsan <ramsudharsanm@gmail.com>

--- drivers/mmc/core/core.c
+++ drivers/mmc/core/core.c
@@ -66,10 +66,10 @@ static const unsigned freqs[] = { 400000, 300000, 200000, 100000 };
 /*
  * Enabling software CRCs on the data blocks can be a significant (30%)
  * performance cost, and for other reasons may not always be desired.
- * So we allow it it to be disabled.
+ * So we allow it to be disabled.
  */
-bool use_spi_crc = 1;
-module_param(use_spi_crc, bool, 0);
+bool use_spi_crc = 0;
+module_param(use_spi_crc, bool, 0644);
 
 /*
  * We normally treat cards as removed during suspend if they are not
