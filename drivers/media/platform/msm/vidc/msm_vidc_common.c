b9d42e1adbc27e817d4d577be45ed1e9a5f4ef32
Balamurugan Alagarsamy <balaga@codeaurora.org>
2015-12-03 15:29:51 +0530

    msm: vidc: remove sync_lock between get & set session property
    Holding sync_lock for the session_get_property and
    session_set_property is leading to stability issues
    and does not seems to be relevant to hold a sync_lock
    here. So, removing it.
    
    CRs-Fixed: 946713
    Change-Id: I2333829c7d1938ba7b4a45de38398cfb6cdd36a1
    Signed-off-by: Balamurugan Alagarsamy <balaga@codeaurora.org>
    Reviewed-on: http://gerrit.mot.com/808325
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Kiran Kumar Krishna <kiran@motorola.com>
    SLTApproved: Kiran Kumar Krishna <kiran@motorola.com>
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/media/platform/msm/vidc/msm_vidc_common.c
+++ drivers/media/platform/msm/vidc/msm_vidc_common.c
@@ -3587,7 +3587,6 @@ int msm_comm_try_get_prop(struct msm_vidc_inst *inst, enum hal_property ptype,
 		return -EAGAIN;
 	}
 	hdev = inst->core->device;
-	mutex_lock(&inst->sync_lock);
 	if (inst->state < MSM_VIDC_OPEN_DONE || inst->state >= MSM_VIDC_CLOSE) {
 		dprintk(VIDC_ERR,
 			"%s Not in proper state\n", __func__);
@@ -3650,7 +3649,6 @@ int msm_comm_try_get_prop(struct msm_vidc_inst *inst, enum hal_property ptype,
 	}
 	mutex_unlock(&inst->pending_getpropq.lock);
 exit:
-	mutex_unlock(&inst->sync_lock);
 	return rc;
 }
 
@@ -3927,7 +3925,6 @@ int msm_comm_try_set_prop(struct msm_vidc_inst *inst,
 	}
 	hdev = inst->core->device;
 
-	mutex_lock(&inst->sync_lock);
 	if (inst->state < MSM_VIDC_OPEN_DONE || inst->state >= MSM_VIDC_CLOSE) {
 		dprintk(VIDC_ERR, "Not in proper state to set property\n");
 		rc = -EAGAIN;
@@ -3938,7 +3935,6 @@ int msm_comm_try_set_prop(struct msm_vidc_inst *inst,
 	if (rc)
 		dprintk(VIDC_ERR, "Failed to set hal property for framesize\n");
 exit:
-	mutex_unlock(&inst->sync_lock);
 	return rc;
 }
 
